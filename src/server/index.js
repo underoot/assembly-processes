fd88ef89f31f51e57b85a53b675d40d0423a41a2
Aleksandr Shoronov <alexshoronov@gmail.com>
2019-11-16 01:00:01 +0300

    Fixed CORS URL

--- src/server/index.js
+++ src/server/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const proxy = require('../setupProxy');
 
-const UI_URL = 'assembly-processes-git-dev.underoot.now.sh';
+const UI_URL = 'https://assembly-processes-git-dev.underoot.now.sh';
 
 const app = express().use((req, res, next) => {
   if (process.env.NODE_ENV === 'production') {
