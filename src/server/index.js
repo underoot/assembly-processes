171b8bf9c9f002b97063d0b592226c4ccd25c22f
Aleksandr Shoronov <alexshoronov@gmail.com>
2019-11-16 00:55:38 +0300

    Added CORS Header in API

--- src/server/index.js
+++ src/server/index.js
@@ -1,4 +1,14 @@
 const express = require('express');
 const proxy = require('../setupProxy');
 
-proxy(express()).listen(process.env.PORT || 3001);
+const UI_URL = 'assembly-processes-git-dev.underoot.now.sh';
+
+const app = express().use((req, res, next) => {
+  if (process.env.NODE_ENV === 'production') {
+    res.set('Access-Control-Allow-Origin', UI_URL);
+  }
+
+  next();
+});
+
+proxy(app).listen(process.env.PORT || 3001);
